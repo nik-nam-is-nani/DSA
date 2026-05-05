004b0ea6361f3dc1593b31ae254f76b60b4bd29b
nik-nam-is-nani <nikshithkyathrigi2005@gmail.com>
2026-05-05 23:25:19 +0530

    leetcode_problem aproach

---
+++ Python foundations/leetcode_right_rotation.py
@@ -0,0 +1,12 @@
+
+
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+n=len(matrix[0])
+for i in range(n):
+    for j in range(i+1,n):
+        matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+for i in range(n):
+            matrix[i].reverse()
+        
