f0fbb599367c5ce0a81ea067731cefcafb8c3441
nik-nam-is-nani <nikshithkyathrigi2005@gmail.com>
2026-06-04 18:15:11 +0530

    Refactor matrix reshape function and add print statement for character 'a'

--- Python foundations/practice.py
+++ Python foundations/practice.py
@@ -185,21 +185,21 @@ for i in range(le):
             mx=max(mx,abs(nums[j]))
             mn=min(mn,abs(nums[j]))
     ans.append(mx-mn)
-return ans
+# return ans
 
-class Solution:
-    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        res = [[0] * c for _ in range(r)]
-        n, m = 0, 0
-        if (len(mat)*len(mat[0])) != r*c:
-            return mat
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                res[n][m] = mat[i][j]
-                m += 1
-                if m >= c:
-                    n+=1
-                    m = 0
-        return res
+# class Solution:
+#     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+#         res = [[0] * c for _ in range(r)]
+#         n, m = 0, 0
+#         if (len(mat)*len(mat[0])) != r*c:
+#             return mat
+#         for i in range(len(mat)):
+#             for j in range(len(mat[0])):
+#                 res[n][m] = mat[i][j]
+#                 m += 1
+#                 if m >= c:
+#                     n+=1
+#                     m = 0
+#         return res
 
-
+print(chr(97))
