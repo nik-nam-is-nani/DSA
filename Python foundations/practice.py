63f590ae80d15d8c95856af36dbc33a88328c2c7
nik-nam-is-nani <nikshithkyathrigi2005@gmail.com>
2026-06-17 18:30:15 +0530

    Update todo list: marked "#1544 Make The String Great" as completed in FUNCTIONS section

--- Python foundations/practice.py
+++ Python foundations/practice.py
@@ -1,205 +1,207 @@
 
 
-# from continue number addition code 
-n=1234
-sum=0
-for i in str(n):
-    sum+=int(i)
-print(sum)
-print("====================================")
-#====================================
+# # from continue number addition code 
+# n=1234
+# sum=0
+# for i in str(n):
+#     sum+=int(i)
+# print(sum)
+# print("====================================")
+# #====================================
 
 
 
-#Reverse a Number
-print("Reverse a Number")
+# #Reverse a Number
+# print("Reverse a Number")
 
-n=1234
-print("before: ",n)
-p=[]
-while n:
-    d=n%10
-    n=n//10
-    p.append(str(d))
-d="".join(p)
+# n=1234
+# print("before: ",n)
+# p=[]
+# while n:
+#     d=n%10
+#     n=n//10
+#     p.append(str(d))
+# d="".join(p)
 
-print(d)
-print("============================\n========================")
-print("==============================================Longest Substring Without Repeating Characters=============================")
+# print(d)
+# print("============================\n========================")
+# print("==============================================Longest Substring Without Repeating Characters=============================")
 
-n="asdfghjklmnvcmnvmnvmnvmncvmn"
-seen=set()
-left=0
-max_len=0
-for right in range(len(n)):
-    while n[right] in seen:
-        seen.remove(n[left])
-        left+=1
-    seen.add(n[right])
-    max_len=max(max_len,right-left+1)
-print(max_len)
+# n="asdfghjklmnvcmnvmnvmnvmncvmn"
+# seen=set()
+# left=0
+# max_len=0
+# for right in range(len(n)):
+#     while n[right] in seen:
+#         seen.remove(n[left])
+#         left+=1
+#     seen.add(n[right])
+#     max_len=max(max_len,right-left+1)
+# print(max_len)
 
 
 
-#=======================================================
-n="asdfghjklmnvcmnvmnvmnvmncvmn"
-seen=set()
-left=0
-max_len=0
-Start=0
-for right in range(len(n)):
-    while n[right] in seen:
-        seen.remove(n[left])
-        left+=1
-    seen.add(n[right])
-    if right-left+1>max_len:
-        max_len=right-left+1
-        Start=left
-print(n[Start:max_len])
-print(max_len)
-print("========================================================")
-#=======================================
-print(" Reverse Words in a String III")
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        op=s.split()
-        va=""
-        for i in range(len(op)):
-            op[i]=op[i][::-1]
+# #=======================================================
+# n="asdfghjklmnvcmnvmnvmnvmncvmn"
+# seen=set()
+# left=0
+# max_len=0
+# Start=0
+# for right in range(len(n)):
+#     while n[right] in seen:
+#         seen.remove(n[left])
+#         left+=1
+#     seen.add(n[right])
+#     if right-left+1>max_len:
+#         max_len=right-left+1
+#         Start=left
+# print(n[Start:max_len])
+# print(max_len)
+# print("========================================================")
+# #=======================================
+# print(" Reverse Words in a String III")
+# class Solution:
+#     def reverseWords(self, s: str) -> str:
+#         op=s.split()
+#         va=""
+#         for i in range(len(op)):
+#             op[i]=op[i][::-1]
           
-        return " ".join(op)
-print("=====================================")
+#         return " ".join(op)
+# print("=====================================")
 
-val="1 23 34 56"
-va=list(map(int,val.split()))
-print(va)
+# val="1 23 34 56"
+# va=list(map(int,val.split()))
+# print(va)
 
-x=int(1234)
-x=str(x)
-x=x[::-1]
-print(x)
-n=14
-print("mass :",n & n-1)
-print(n & (n-1)==0)
-n=12
-i=1
-a="11"
-b="1"
-print("new")
-
-
+# x=int(1234)
+# x=str(x)
+# x=x[::-1]
+# print(x)
+# n=14
+# print("mass :",n & n-1)
+# print(n & (n-1)==0)
+# n=12
+# i=1
+# a="11"
+# b="1"
+# print("new")
 
 
-a1=int(a,2)
-b1=int(b,2)
-a1=a1+b1
-a1=bin(a1)
-a1=a1[2:]
-a1=str(a1)
-print(a1)
+
+
+# a1=int(a,2)
+# b1=int(b,2)
+# a1=a1+b1
+# a1=bin(a1)
+# a1=a1[2:]
+# a1=str(a1)
+# print(a1)
         
 
-res="4"==4
+# res="4"==4
 
-# print(res)
-# mass="abs23"
-# print(any(mass.isalpha()))
+# # print(res)
+# # mass="abs23"
+# # print(any(mass.isalpha()))
 
-# class Solution:
-#     def maximumValue(self, strs: List[str]) -> int:
-#         max_val = 0
-#         for s in strs:
-#             max_val = max(self.get_value(s), max_val)
-#         return max_val
-#     def get_value(self,s:str):
-#         for idx, c in enumerate(s):
-#             if c.isalpha():
-#                 return len(s)
+# # class Solution:
+# #     def maximumValue(self, strs: List[str]) -> int:
+# #         max_val = 0
+# #         for s in strs:
+# #             max_val = max(self.get_value(s), max_val)
+# #         return max_val
+# #     def get_value(self,s:str):
+# #         for idx, c in enumerate(s):
+# #             if c.isalpha():
+# #                 return len(s)
 
-a="K"
-c="d"
-b=ord(a)+1
-print(f"{chr(b)}{c}")
+# a="K"
+# c="d"
+# b=ord(a)+1
+# print(f"{chr(b)}{c}")
 
-names = ["Mary","John","Emma"]
-heights = [180,165,170]
+# names = ["Mary","John","Emma"]
+# heights = [180,165,170]
 
-sor=sorted(heights,reverse=True)
-res=[]
-for i in range(len(sor)):
-    res.append(names[heights.index(sor[i])])
-    print(res)
-
-
+# sor=sorted(heights,reverse=True)
+# res=[]
+# for i in range(len(sor)):
+#     res.append(names[heights.index(sor[i])])
+#     print(res)
 
 
 
-peo = [
-    ["nik", 21],
-    ["ram", 18],
-    ["sai", 25]
-]
-
-peo.sort(key=lambda x:x[1], reverse=True)
 
 
-#used  when we are implementing the zip funticons in the dsa 
-startTime = [1,2,3]
-endTime = [3,2,7]
-queryTime = 4
-c=0
-for i in range(len(startTime)):
-    if startTime[i]-endTime[i]==queryTime:
-        c+=1
-    print(startTime[i]-endTime[i])
-s="aaba"
+# peo = [
+#     ["nik", 21],
+#     ["ram", 18],
+#     ["sai", 25]
+# ]
+
+# peo.sort(key=lambda x:x[1], reverse=True)
 
 
-le=len(s)-1
-i=0
-b=False
-a=False
-while i<=le:
-    print("val B ",b)
-    print("val A ",a)
-    if s[i]=='a' and b==False:
-        a=True
-        i+=1
-    elif s[i]=='b':
-        print("massssss")
-        b=True
-        i+=1
-    else:
-        print(False)
-        i+=1
-print(True)
+# #used  when we are implementing the zip funticons in the dsa 
+# startTime = [1,2,3]
+# endTime = [3,2,7]
+# queryTime = 4
+# c=0
+# for i in range(len(startTime)):
+#     if startTime[i]-endTime[i]==queryTime:
+#         c+=1
+#     print(startTime[i]-endTime[i])
+# s="aaba"
 
-nums =[87063,61094,44530,21297,95857,93551,9918]
-ans=[]
-le=len(nums)
-mx=float('-inf')
-mn=float('inf')
-for i in range(le):
-    for j in range(le):
-        if j!=i:
-            mx=max(mx,abs(nums[j]))
-            mn=min(mn,abs(nums[j]))
-    ans.append(mx-mn)
-# return ans
 
-# class Solution:
-#     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-#         res = [[0] * c for _ in range(r)]
-#         n, m = 0, 0
-#         if (len(mat)*len(mat[0])) != r*c:
-#             return mat
-#         for i in range(len(mat)):
-#             for j in range(len(mat[0])):
-#                 res[n][m] = mat[i][j]
-#                 m += 1
-#                 if m >= c:
-#                     n+=1
-#                     m = 0
-#         return res
+# le=len(s)-1
+# i=0
+# b=False
+# a=False
+# while i<=le:
+#     print("val B ",b)
+#     print("val A ",a)
+#     if s[i]=='a' and b==False:
+#         a=True
+#         i+=1
+#     elif s[i]=='b':
+#         print("massssss")
+#         b=True
+#         i+=1
+#     else:
+#         print(False)
+#         i+=1
+# print(True)
 
-print(chr(97))
+# nums =[87063,61094,44530,21297,95857,93551,9918]
+# ans=[]
+# le=len(nums)
+# mx=float('-inf')
+# mn=float('inf')
+# for i in range(le):
+#     for j in range(le):
+#         if j!=i:
+#             mx=max(mx,abs(nums[j]))
+#             mn=min(mn,abs(nums[j]))
+#     ans.append(mx-mn)
+# # return ans
+
+# # class Solution:
+# #     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+# #         res = [[0] * c for _ in range(r)]
+# #         n, m = 0, 0
+# #         if (len(mat)*len(mat[0])) != r*c:
+# #             return mat
+# #         for i in range(len(mat)):
+# #             for j in range(len(mat[0])):
+# #                 res[n][m] = mat[i][j]
+# #                 m += 1
+# #                 if m >= c:
+# #                     n+=1
+# #                     m = 0
+# #         return res
+
+# print(chr(97))
+
+print(ord('P'))
